380ded665a62f61d03a047b750dc92335201c3a3
Graham Smith <grahamas@gmail.com>
2021-02-02 13:38:07 -0700

    Update to v0.15 Makie

--- src/ensembles.jl
+++ src/ensembles.jl
@@ -18,14 +18,14 @@ function sweep_2D_slice_heatmaps(scene, A::NamedAxisArray;
         
         @assert size(mean_values) == length.((mod_values[x], mod_values[y]))
         
-        layout[x,y] = ax = LAxis(scene); 
+        layout[x,y] = ax = MakieLayout.Axis(scene); 
         tightlimits!(ax)
         heatmap!(ax, my, mx, mean_values', colorrange=(0,1), color=plot_color)
             #xlab=mod_names[y], ylab=mod_names[x], color=plot_color, title="prop epileptic"),
     end
-    layout[:,1] = LText.(Ref(scene), mod_names[1:end-1], tellheight=false, rotation=pi/2)
-    layout[end+1,2:end] = LText.(scene, mod_names[2:end], tellwidth=false)
-    layout[0, :] = LText(scene, title, textsize = 30)
+    layout[:,1] = Label.(Ref(scene), mod_names[1:end-1], tellheight=false, rotation=pi/2)
+    layout[end+1,2:end] = Label.(scene, mod_names[2:end], tellwidth=false)
+    layout[0, :] = Label(scene, title, textsize = 30)
     cbar = layout[2:end-1, end+1] = LColorbar(scene, heatmaps[1], label = "Proportion classified")
     cbar.width = 25
     return layout
--- src/executions.jl
+++ src/executions.jl
@@ -14,7 +14,7 @@ function heatmap_slices_execution(exec::AbstractExecution, n_slices=5, resolutio
     step = (length(soln.t)) ÷ n_slices
     t_idxs = 2:step:length(soln.t)
     
-    hm_axes = [LAxis(scene, title = "$pop_name activity", aspect=1.0) for pop_name in pop_names]
+    hm_axes = [MakieLayout.Axis(scene, title = "$pop_name activity", aspect=1.0) for pop_name in pop_names]
     t_idx_subs = 1:max(1,(length(t) ÷ max_ts)):length(t) 
     xs_idx_subs = 1:max(1,(length(xs) ÷ max_xs)):length(xs) 
     heatmaps = map(1:length(pop_names)) do idx_pop
@@ -29,7 +29,7 @@ function heatmap_slices_execution(exec::AbstractExecution, n_slices=5, resolutio
     layout[2,pop_idxs] = map(pop_idxs) do pop_idx
         slices_layout = GridLayout(rowsizes=[Auto()], alignmode=Outside(10), tellheight=true)
         slice_axes = slices_layout[:h] = map(t_idxs) do t_idx
-            ax = LAxis(scene, aspect=1.0, tellheight=true)
+            ax = MakieLayout.Axis(scene, aspect=1.0, tellheight=true)
             lines!(ax, xs[xs_idx_subs], soln[xs_idx_subs,pop_idx,t_idx])
             tightlimits!(ax)
             hideydecorations!(ax)
@@ -37,7 +37,7 @@ function heatmap_slices_execution(exec::AbstractExecution, n_slices=5, resolutio
             ax
         end
         linkaxes!(slice_axes...)
-        slices_layout[2,1:length(t_idxs)] = [LText(scene, "t=$(round(time, digits=1))", textsize=14, tellwidth=false) for time in t[t_idxs]]
+        slices_layout[2,1:length(t_idxs)] = [Label(scene, "t=$(round(time, digits=1))", textsize=14, tellwidth=false) for time in t[t_idxs]]
         trim!(slices_layout)
         slices_layout        
     end
@@ -45,8 +45,8 @@ function heatmap_slices_execution(exec::AbstractExecution, n_slices=5, resolutio
     cbar = layout[1, end+1] = LColorbar(scene, heatmaps[1], label = "Activity Level")
     cbar.width = 25
 
-    ylabel = layout[:,0] = LText(scene, "space (μm)", rotation=pi/2, tellheight=false)
-    xlabel = layout[end+1,2:3] = LText(scene, "time (ms)")
+    ylabel = layout[:,0] = Label(scene, "space (μm)", rotation=pi/2, tellheight=false)
+    xlabel = layout[end+1,2:3] = Label(scene, "time (ms)")
     return (scene, layout)
 end
 
@@ -63,7 +63,7 @@ function animate_execution(filename::AbstractString, execution::AbstractFullExec
     time_idx_node = Node(1)
     single_pop = lift(idx -> population_timepoint(solution, 1, idx), time_idx_node)
     title = lift(idx -> "t = $(t[idx])", time_idx_node)
-    ax = layout[1,1] = LAxis(scene, title=title)
+    ax = layout[1,1] = MakieLayout.Axis(scene, title=title)
     lines!(ax, x, single_pop)
     @show single_pop
     @show x
@@ -90,8 +90,8 @@ end
 #     single_pop = lift(idx -> population_timepoint(solution, 1, idx), time_idx_node)
 #     title = lift(idx -> "t = $(t[idx])", time_idx_node)
 #     @assert size(single_pop[]) == (length(x), length(y))
-#     # layout[1,1] = LText(scene, title, tellwidth=false)
-#     layout[1,1] = ax = LAxis(scene, title=title)
+#     # layout[1,1] = Label(scene, title, tellwidth=false)
+#     layout[1,1] = ax = MakieLayout.Axis(scene, title=title)
 #     heatmap!(ax, x, y, single_pop, colorrange=(min_val,max_val))
     
 #     record(scene, filename, 1:length(t); framerate=fps) do time_idx # TODO @views
@@ -114,7 +114,7 @@ function exec_heatmap!(scene::Scene, exec::AbstractExecution;
     xs = coordinate_axes(Simulation73.reduced_space(exec))[1] |> collect
     pop_names = exec.simulation.model.pop_names
 
-    hm_axes = layout[1:length(pop_names), 1] = [LAxis(scene; axis_kwargs...) for pop_name in pop_names]
+    hm_axes = layout[1:length(pop_names), 1] = [MakieLayout.Axis(scene; axis_kwargs...) for pop_name in pop_names]
     u_max = maximum(maximum.(soln.u))
     u_min = minimum(minimum.(soln.u))
     clims = if u_max == u_min
@@ -141,12 +141,12 @@ function exec_heatmap!(scene::Scene, exec::AbstractExecution;
     cbar = layout[length(pop_names),2] = LColorbar(scene, heatmaps[begin], width=colorbar_width, vertical=true)
 
     if title !== nothing
-        layout[0,:] = LText(scene, title, tellwidth=false)
+        layout[0,:] = Label(scene, title, tellwidth=false)
     end
   
     if !no_labels
-        ylabel = layout[2,0] = LText(scene, "space (μm)", rotation=pi/2, tellheight=false)
-        xlabel = layout[end+1,2] = LText(scene, "time (ms)", tellwidth=false)
+        ylabel = layout[2,0] = Label(scene, "space (μm)", rotation=pi/2, tellheight=false)
+        xlabel = layout[end+1,2] = Label(scene, "time (ms)", tellwidth=false)
     end
     return layout
 end 
