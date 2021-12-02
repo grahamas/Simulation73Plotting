b2b6ace3d8cc950e9a7f1fa60320c03ec119a433
Graham Smith <grahamas@gmail.com>
2021-12-02 14:02:37 -0700

    Plot parent of nda

--- src/ensembles.jl
+++ src/ensembles.jl
@@ -1,5 +1,5 @@
 
-function sweep_2D_slice_heatmaps(scene, A::NamedDimsArray, A_dims::NamedTuple; 
+function sweep_2D_slice_heatmaps(fig::Figure, A::NamedDimsArray, A_dims::NamedTuple; 
                        plot_color = :magma, title = "FILLER")
     mod_names = [string(name) for name in keys(A_dims)]
     mod_values = values(A_dims)
@@ -18,15 +18,15 @@ function sweep_2D_slice_heatmaps(scene, A::NamedDimsArray, A_dims::NamedTuple;
         
         @assert size(mean_values) == length.((mod_values[x], mod_values[y]))
         
-        layout[x,y] = ax = MakieLayout.Axis(scene); 
+        layout[x,y] = ax = MakieLayout.Axis(fig); 
         tightlimits!(ax)
-        heatmap!(ax, my, mx, mean_values', colorrange=(0,1), color=plot_color)
+        heatmap!(ax, my, mx, parent(mean_values)', colorrange=(0,1), color=plot_color)
             #xlab=mod_names[y], ylab=mod_names[x], color=plot_color, title="prop epileptic"),
     end
-    layout[:,1] = Label.(Ref(scene), mod_names[1:end-1], tellheight=false, rotation=pi/2)
-    layout[end+1,2:end] = Label.(scene, mod_names[2:end], tellwidth=false)
-    layout[0, :] = Label(scene, title, textsize = 30)
-    cbar = layout[2:end-1, end+1] = Colorbar(scene, heatmaps[1], label = "Proportion classified")
+    layout[:,1] = Label.(Ref(fig), mod_names[1:end-1], tellheight=false, rotation=pi/2)
+    layout[end+1,2:end] = Label.(fig, mod_names[2:end], tellwidth=false)
+    layout[0, :] = Label(fig, title, textsize = 30)
+    cbar = layout[2:end-1, end+1] = Colorbar(fig, heatmaps[1], label = "Proportion classified")
     cbar.width = 25
     return layout
 end
